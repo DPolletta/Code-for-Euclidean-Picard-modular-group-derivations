92e2227321e539a0c0dee80e2962eb0d30f750db
DPolletta <DPolletta@users.noreply.github.com>
2019-08-26 13:12:49 -0700

    Update generators.m

--- generators.m
+++ generators.m
@@ -1,3 +1,5 @@
+%this script sets up generators for cusp stabilizer Gamma_infty(2)
+
 J = [0 0 1; 0 1 0; 1 0 0];  %matrix for Hermitian form
 
 T2 = [1 -2 -2; 0 1 2; 0 0 1];  %generators for cusp stabilizer d=2
