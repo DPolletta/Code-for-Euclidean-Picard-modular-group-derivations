190a00d831835d285d1a86c31763a78a87c856ca
DPolletta <DPolletta@users.noreply.github.com>
2019-08-26 11:03:36 -0700

    Add files via upload

---
+++ bound.m
@@ -0,0 +1,4 @@
+function Y = bound(pa,pb,pc)
+Y = (((4*dep(pc))/(dep(pa)*dep(pb)))^(1/4)); %this function computes the Cygan distance bound between
+end                                          % the points A_a^-1(infinity) and \gamma(p_b) 
+                                             %(or whichever case we are in)
