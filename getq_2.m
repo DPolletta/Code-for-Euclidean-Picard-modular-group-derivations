8b688f6badcd9963cdef29364aab39e2501f67f1
DPolletta <DPolletta@users.noreply.github.com>
2019-08-26 11:45:34 -0700

    Add files via upload

---
+++ getq_2.m
@@ -0,0 +1,16 @@
+%for d=2, this script computes the possible scale factor, q, for a
+%rational point in the boundary we obtain the bounds -100:100 by graphing
+%the norm function and checking the max x,y values in the ellipse
+%we only take values of q up to multiplication by a unit +/- 1
+for q = 1:45
+for a = -100:100
+    for b = -100:100
+        
+        if norm((norm(cmp(a,b))^2 - q)) <= 0.00001
+            fprintf('q = %d, %d + %d(isqrt(2))\n',q,a,b)
+        else
+            continue
+        end
+    end
+end
+end
